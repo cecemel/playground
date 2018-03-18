70ac2f4196b500e9baba1dea36b6e52cc99a8689
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-03-18 21:02:01 +0100

    added custom log function

--- cat-food-dispenser/dispenser.py
+++ cat-food-dispenser/dispenser.py
@@ -17,16 +17,16 @@ def serve_food():
         for position in steps:
             os.system("fast-gpio pwm 1 50 {}".format(position))
             time.sleep(1)
-            print "reached position {}".format(position)
+            dispenser_log("reached position {}".format(position))
 
-        print "reached end position, waiting {} seconds...".format(wait_time)
+        dispenser_log("reached end position, waiting {} seconds...".format(wait_time))
         time.sleep(wait_time)
         everything_ok = True
 
     #TODO: try recover error
 
     finally:
-        print "going back to start position"
+        dispenser_log("going back to start position")
         os.system("fast-gpio pwm 1 50 {}".format(start_position))
         time.sleep(wait_time)
         os.system("fast-gpio set 1 0")
@@ -38,13 +38,10 @@ def control_food_serving(target_time_string):
     sleep_time = 10
     last_served_food_at = None
     target_time = parse_time_string(target_time_string)
-
     while(True):
-        print("sleeping {} seconds".format(sleep_time))
         time.sleep(sleep_time)
-
         if not had_food_today(last_served_food_at) and is_time_to_eat(target_time):
-            print("serving food")
+            dispenser_log("serving food at {}".format(datetime.datetime.now()))
             was_it_ok = serve_food()
             if was_it_ok:
                 last_served_food_at = datetime.datetime.now()
@@ -70,7 +67,11 @@ def had_food_today(last_food_served):
 def parse_time_string(target_time_string):
     return datetime.datetime.strptime(target_time_string, '%H:%M')
 
+def dispenser_log(content_string):
+    print content_string
+    sys.stdout.flush()
+
 if __name__ == "__main__":
     time_to_eat = sys.argv[1]
-    print("Will eat at {}".format(time_to_eat))
+    dispenser_log("Will eat at {}".format(time_to_eat))
     control_food_serving(time_to_eat)
